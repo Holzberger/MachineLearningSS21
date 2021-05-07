5ca72e0a2d4b6f96de6658f270128baa4fe65b8c
fabian <fabian-holzberger@live.de>
2021-05-08 01:54:47 +0200

    added 1d code for model tree

---
+++ ex2/algorithms/tree_tests1d.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  7 16:58:14 2021
+
+@author: fabian
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tree import *
+
+def f1(n_samples):
+    x = np.random.uniform(-1, 1, n_samples)
+    noise = np.random.normal(0, 0.5, n_samples)
+    y = noise+x**3 - 4*x**2 + x +1+ np.sin(10*x)
+    return x,y
+
+x,y = f1(100)
+
+plt.plot(x,y,".")
+
+data = np.stack((x, y)).T
+
+root = create_M5(data)
+
+# draw model
+X = np.linspace(-1,1,100)
+Y=np.array([predict(root, elem) for elem in X])
+plt.plot(X,Y,linewidth=2,color="red")
+
+# draw splits
+splits = print_split(root)
+for split in splits:
+    plt.axvline(x=split,color="orange",linestyle="--",linewidth=1)
