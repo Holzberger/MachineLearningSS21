c4b03f73546cf65ffbcf979679e50d458b784e2a
fabian <fabian-holzberger@live.de>
2021-05-22 16:25:58 +0200

    added lin tree

--- ex2/algorithms/traffic_volume_test.py
+++ ex2/algorithms/traffic_volume_test.py
@@ -18,6 +18,8 @@ from sklearn.neighbors import KNeighborsRegressor
 
 from treend import *
 from reg_tree import *
+from sklearn.linear_model import *
+from lineartree import LinearTreeClassifier, LinearTreeRegressor
 
 #%%
 
@@ -54,27 +56,27 @@ y_train = np.array(y_train).astype("float")
 y_test = np.array(y_test).astype("float")
 x_train,x_vali,y_train,y_vali = train_test_split(x_train,y_train,test_size = 0.2,random_state=42)
 #%%
-from sklearn.metrics import mean_absolute_error
-#reg0 = Const_regressor(n_attr_leaf=4, max_depth=15,smoothing=False)
-reg0 = Const_regressor(n_attr_leaf=4, max_depth=15,smoothing=False)
-reg0.fit(x_train,y_train[:,None])
-#%%
-reg0.prune(x_vali,y_vali[:,None])
-#%%
-reg0.smoothing=True
-reg0.k=0.5
-predictions = reg0.predict(x_test)
-print("mean_absolute_error is : " , mean_absolute_error(y_test, predictions))
-print("mean target :", np.mean(y))
+# from sklearn.metrics import mean_absolute_error
+# #reg0 = Const_regressor(n_attr_leaf=4, max_depth=15,smoothing=False)
+# reg0 = Const_regressor(n_attr_leaf=4, max_depth=15,smoothing=False)
+# reg0.fit(x_train,y_train[:,None])
+# #%%
+# reg0.prune(x_vali,y_vali[:,None])
+# #%%
+# reg0.smoothing=True
+# reg0.k=0.5
+# predictions = reg0.predict(x_test)
+# print("mean_absolute_error is : " , mean_absolute_error(y_test, predictions))
+# print("mean target :", np.mean(y))
 #%%
 
-from sklearn.model_selection import cross_val_score
-reg1 = M5regressor(n_attr_leaf=4, max_depth=15)
-reg1.incremental_fit=False
-reg1.fit(x_train,y_train[:,None])
-#%%
-optimize_models=True
-reg1.prune(x_vali,y_vali[:,None])
+# from sklearn.model_selection import cross_val_score
+# reg1 = M5regressor(n_attr_leaf=4, max_depth=15)
+# reg1.incremental_fit=False
+# reg1.fit(x_train,y_train[:,None])
+# #%%
+# optimize_models=True
+# reg1.prune(x_vali,y_vali[:,None])
 #%%
 # reg1.smoothing=True
 # reg1.k=200.0
@@ -99,6 +101,7 @@ reg1.prune(x_vali,y_vali[:,None])
 
 
 
-
+reg3 = LinearTreeRegressor(base_estimator=LinearRegression())
+reg3.fit(x_train,y_train)  # supports also multi-target and sample_weights
 
 
